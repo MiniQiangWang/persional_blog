74cebed8029b5e51e3a721f8200070686712e1df
MiniQiangWang <823980367@qq.com>
2024-11-03 21:42:19 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+    <!-- <h1>This is an about page</h1> -->
+  </div>
+</template>
+
+<script>
+  const rawHtml = `<span style="color: red">This should be red.</span>`;
+
+  export {
+    rawHtml
+  }
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
